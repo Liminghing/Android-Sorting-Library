8ebebb8e2f3aaccc9b3f48cff10cf0823fdbd84b
Liminghing <im349412@gmail.com>
2024-10-31 22:49:05 +0900

    edit insert sorting

--- app/src/main/java/com/myqr/kotlinsorting/InsertionSorting.kt
+++ app/src/main/java/com/myqr/kotlinsorting/InsertionSorting.kt
@@ -1,4 +1,46 @@
 package com.myqr.kotlinsorting
 
-class InsertionSorting {
+public class InsertionSorting {
+    /**
+     *  정수를 오름차순으로 삽입정렬하는 함수
+     *
+     *  @param arr  정렬하고자하는 ArrayList<Int>배열
+     *  @return 정렬된 ArrayList<Int> 반환
+     */
+    public fun insertionSortingIntUp(arr :  ArrayList<Int>) :  ArrayList<Int> {
+        var n = arr.size
+        for (i in 1 until n){
+            var key = arr[i]
+            for (j in 0 until (i-1) ){
+                if(arr[i] > key){
+                    arr[j+1] = arr[j]
+                }else{
+                    break
+                }
+            }
+            arr[i] = key
+        }
+        return arr
+    }
+
+    /**
+     *  정수를 내림차순 삽입정렬하는 함수
+     *
+     *  @param arr  정렬하고자하는 ArrayList<Int>배열
+     *  @return 정렬된 ArrayList<Int> 반환
+     */
+    public fun insertionSortingIntDown(arr :  ArrayList<Int>) :  ArrayList<Int> {
+        var n = arr.size
+        for (i in 0 until n){
+            for (j in 0 until (n-1)){
+                if(arr[j]<arr[j+1]){
+                    var temp = arr[j]
+                    arr[j] = arr[j+1]
+                    arr[j+1] = temp
+                }
+            }
+        }
+        return arr
+    }
+
 }
