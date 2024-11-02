2097712a6d006177caa0e8b38563da4ac78f0108
Liminghing <im349412@gmail.com>
2024-11-02 17:34:17 +0900

    add selection sorting

--- app/src/main/java/com/myqr/kotlinsorting/SelectionSorting.kt
+++ app/src/main/java/com/myqr/kotlinsorting/SelectionSorting.kt
@@ -1,4 +1,52 @@
 package com.myqr.kotlinsorting
 
-class SelectionSorting {
+public class SelectionSorting {
+    /**
+     *  정수를 오름차순으로 선택정렬하는 함수
+     *
+     *  @param arr  정렬하고자하는 ArrayList<Int>배열
+     *  @return 정렬된 ArrayList<Int> 반환
+     */
+    public fun selectionSortingIntUp(arr :  ArrayList<Int>) :  ArrayList<Int> {
+        var n = arr.size
+        for (i in 0 until n){
+            var min = arr[i]
+            var index = i
+            for (j in i+1 until  n ) {
+                if (min > arr[j]) {
+                    min = arr[j]
+                    index = j
+                }
+                var temp = arr[i]
+                arr[i] = arr[index]
+                arr[index] = temp
+            }
+        }
+        return arr
+    }
+
+    /**
+     *  정수를 내림차순 선택정렬하는 함수
+     *
+     *  @param arr  정렬하고자하는 ArrayList<Int>배열
+     *  @return 정렬된 ArrayList<Int> 반환
+     */
+    public fun selectionSortingIntDown(arr :  ArrayList<Int>) :  ArrayList<Int> {
+        var n = arr.size
+        for (i in 0 until n){
+            var max = arr[i]
+            var index = i
+            for (j in i+1 until  n ) {
+                if (max < arr[j]) {
+                    max = arr[j]
+                    index = j
+                }
+                var temp = arr[i]
+                arr[i] = arr[index]
+                arr[index] = temp
+            }
+        }
+        return arr
+    }
+
 }
